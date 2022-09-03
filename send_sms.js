39e9600651fbb88fb412b04186c3de9e5ad34ff1
spykid-111 <rrsingh1101@gmail.com>
2022-09-03 16:51:57 +0530

    twilio-setup

---
+++ send_sms.js
@@ -0,0 +1,13 @@
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilio = require('twilio');
+const client = twilio(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'Hello Ritu!!, Siddhant needs your help. His location:- ',
+     from: '+19517087951',
+     to: '+919661591407'
+   })
+  .then(message => console.log(message.sid));
